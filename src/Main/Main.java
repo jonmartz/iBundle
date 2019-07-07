19b2cbf7e4c0092ef230ff2460d253a1733dbc5f
jonmartz <jonmartz91@gmail.com>
2019-07-07 15:14:08 +0300

    Two options in main: random or manual start and goal nodes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -28,103 +28,41 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 800 , 540));
         primaryStage.show();
 
-        //main
-
-//        List<Agent> list = new ArrayList<>();
-//        list.add(null);
-//        list.add(null);
-//        list.add(null);
-//        list.add(null);
-//        list.add(null);
-//        list.add(null);
-//
-//        IWinnerDeterminator iWinnerDeterminator = new WinnerDeterminator();
-//        iWinnerDeterminator.getWinners(list);
-
-//        // simple test
-//        GridGraph graph = new GridGraph("./Resources/test.map");
-//        Agent agent1 = new Agent(graph.nodes.get(1), graph.nodes.get(3), new AStarSearcher(), graph);
-
-        // big test
         // create graph from map
-        GridGraph graph = new GridGraph("./Resources/brc000d.map");
-        HashSet<Node> startAndGoalNodes = new HashSet<>();
-
-        // Get random start and goal states from graph
-        GridNode nodeA = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeA);
-        GridNode nodeB = (GridNode)graph.getRandomNode();
-        while (startAndGoalNodes.contains(nodeB)) nodeB = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeB);
-        GridNode nodeC = (GridNode)graph.getRandomNode();
-        while (startAndGoalNodes.contains(nodeC)) nodeC = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeC);
-        GridNode nodeD = (GridNode)graph.getRandomNode();
-        while (startAndGoalNodes.contains(nodeD)) nodeD = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeD);
-        GridNode nodeE = (GridNode)graph.getRandomNode();
-        while (startAndGoalNodes.contains(nodeE)) nodeE = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeE);
-        GridNode nodeF = (GridNode)graph.getRandomNode();
-        while (startAndGoalNodes.contains(nodeF)) nodeF = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeF);
-        GridNode nodeG = (GridNode)graph.getRandomNode();
-        while (startAndGoalNodes.contains(nodeG)) nodeG = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeG);
-        GridNode nodeI = (GridNode)graph.getRandomNode();
-        while (startAndGoalNodes.contains(nodeI)) nodeI = (GridNode)graph.getRandomNode();
-        startAndGoalNodes.add(nodeI);
-
-//        // create nodes
-//        GridNode nodeA = new GridNode(0, 0,0);
-//        GridNode nodeB = new GridNode(1, 0,0);
-//        GridNode nodeC = new GridNode(2,0,0);
-//        GridNode nodeD = new GridNode(3,0,0);
-//        GridNode nodeE = new GridNode(4,0,0);
-//        GridNode nodeF = new GridNode(5,0,0);
-//        GridNode nodeG = new GridNode(6,0,0);
-//        GridNode nodeH = new GridNode(7,0,0);
-//        GridNode nodeI = new GridNode(8,0,0);
-//
-//        // connect nodes
-//        nodeA.addNeighbor(nodeB);//0-1
-//        nodeA.addNeighbor(nodeC);//0-2
-//        nodeB.addNeighbor(nodeD);//1-3
-//        nodeC.addNeighbor(nodeD);//2-3
-//        nodeC.addNeighbor(nodeE);//2-4
-//        nodeD.addNeighbor(nodeF);//3-5
-//        nodeD.addNeighbor(nodeG);//3-6
-//        nodeE.addNeighbor(nodeG);//4-6
-//        nodeE.addNeighbor(nodeI);//4-8
-//        nodeF.addNeighbor(nodeH);//5-7
-//
-//        // add nodes to graph
-//        Graph graph = new Graph();
-//        graph.addNode(nodeA);
-//        graph.addNode(nodeB);
-//        graph.addNode(nodeC);
-//        graph.addNode(nodeD);
-//        graph.addNode(nodeE);
-//        graph.addNode(nodeF);
-//        graph.addNode(nodeG);
-//        graph.addNode(nodeH);
-//        graph.addNode(nodeI);
-
-        // create agents
-        Agent agent1 = new Agent(graph.nodes.get(2), graph.nodes.get(15), new BFSearcher(), graph);
-        Agent agent2 = new Agent(graph.nodes.get(10), graph.nodes.get(13), new BFSearcher(), graph);
-        Agent agent3 = new Agent(graph.nodes.get(5), graph.nodes.get(3), new BFSearcher(), graph);
-        Agent agent4 = new Agent(graph.nodes.get(22), graph.nodes.get(16), new BFSearcher(), graph);
-        Agent agent5 = new Agent(graph.nodes.get(24), graph.nodes.get(5), new BFSearcher(), graph);
-        Agent agent6 = new Agent(graph.nodes.get(21), graph.nodes.get(7), new BFSearcher(), graph);
-
+        GridGraph graph = new GridGraph("./Resources/den312d.map");
         ArrayList<Agent> agents = new ArrayList<>();
-        agents.add(agent1);
-        agents.add(agent2);
-        agents.add(agent3);
-        agents.add(agent4);
-        agents.add(agent5);
-        agents.add(agent6);
+
+        // 1) random start and goals
+        int numAgents = 10;
+        HashSet<GridNode> startAndGoalNodes = new HashSet<>();
+        GridNode node;
+        for (int i = 0; i < numAgents*2; i++){
+            node = (GridNode) graph.getRandomNode();
+            while (startAndGoalNodes.contains(node))
+                node = (GridNode)graph.getRandomNode();
+            startAndGoalNodes.add(node);
+        }
+        int j = 0;
+        Node startNode = null;
+        for (GridNode gridNode : startAndGoalNodes){
+            if (j % 2 == 0) startNode = gridNode;
+            else agents.add(new Agent(startNode, gridNode, new BFSearcher(), graph));
+            j++;
+        }
+
+//        // 2) manual start and goal nodes
+//        Agent agent1 = new Agent(graph.nodes.get(2), graph.nodes.get(15), new BFSearcher(), graph);
+//        Agent agent2 = new Agent(graph.nodes.get(10), graph.nodes.get(13), new BFSearcher(), graph);
+//        Agent agent3 = new Agent(graph.nodes.get(5), graph.nodes.get(3), new BFSearcher(), graph);
+//        Agent agent4 = new Agent(graph.nodes.get(22), graph.nodes.get(16), new BFSearcher(), graph);
+//        Agent agent5 = new Agent(graph.nodes.get(24), graph.nodes.get(5), new BFSearcher(), graph);
+//        Agent agent6 = new Agent(graph.nodes.get(21), graph.nodes.get(7), new BFSearcher(), graph);
+//        agents.add(agent1);
+//        agents.add(agent2);
+//        agents.add(agent3);
+//        agents.add(agent4);
+//        agents.add(agent5);
+//        agents.add(agent6);
 
         // create auction
         Auction auction = new Auction(1, new WinnerDeterminator());
@@ -132,8 +70,6 @@ public class Main extends Application {
         // run
         while(!auction.finished){
             iteration++;
-
-
 
             // STAGE 1 - bidding
             for (Agent agent : agents) {
